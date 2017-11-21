6f901ff1e21cf26ac25ca897b183749c0d65948d
Harshad Deshmukh <hbdeshmukh@apache.org>
2017-11-20 22:14:18 -0600

    Added basic pipeline annotation support

---
+++ query_execution/DAGAnalyzer.cpp
@@ -0,0 +1,248 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ **/
+
+#include "query_execution/DAGAnalyzer.hpp"
+
+#include <algorithm>
+#include <cstddef>
+#include <iostream>
+#include <memory>
+#include <queue>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include "utility/DAG.hpp"
+
+#include "glog/logging.h"
+
+namespace quickstep {
+
+void DAGAnalyzer::findPipelines() {
+  // Key = node ID, value = whether the node has been visited or not.
+  std::unordered_map<std::size_t, bool> visited_nodes;
+  // Pair 1st element: Operator ID
+  // Pair 2nd element: The index of the pipeline in pipelines_ vector to which
+  // the 1st element belongs.
+  std::queue<std::pair<std::size_t, std::size_t>> to_be_checked_nodes;
+  // Traverse the DAG once. Find leaf nodes and create a pipeline with one node.
+  for (std::size_t node_id = 0; node_id < query_plan_dag_->size(); ++node_id) {
+    if (query_plan_dag_->getDependencies(node_id).empty()) {
+      pipelines_.push_back(std::unique_ptr<Pipeline>(new Pipeline(node_id)));
+      to_be_checked_nodes.emplace(node_id, pipelines_.size() - 1);
+      // No check needed here, as we are visiting these nodes for the first time.
+      visited_nodes[node_id] = true;
+    }
+  }
+  while (!to_be_checked_nodes.empty()) {
+    // Get the next node to be checked.
+    auto queue_front = to_be_checked_nodes.front();
+    to_be_checked_nodes.pop();
+    const std::size_t pipeline_id = queue_front.second;
+    const std::size_t operator_id = queue_front.first;
+
+    // Make sure this node has already been visited.
+    DCHECK(visited_nodes.end() != visited_nodes.find(operator_id));
+
+    // Get all the dependents of the operator.
+    // This is a list of pairs, in which first element is the operator ID of the
+    // dependent and second element is a boolean.
+    // If the boolean is true, that means the link between the operators is
+    // pipeline breaker, otherwise it is pipeline-able.
+    auto dependents = query_plan_dag_->getDependents(operator_id);
+    DLOG(INFO) << "Scanning dependents of " << operator_id << std::endl;
+
+    // Traverse over all the dependents ...
+    for (auto dependent_pair : dependents) {
+      const std::size_t dependent_id = dependent_pair.first;
+      // We create a new pipeline for the dependent if at least one of the two
+      // condition holds:
+      // 1. There are more than one dependencies of the given node.
+      // 2. The edge between two nodes is pipeline-breaking.
+      const bool dependent_in_new_pipeline =
+          (query_plan_dag_->getDependencies(dependent_id).size() > 1u) ||
+          (dependent_pair.second);
+      DLOG(INFO) << "Dependent ID: " << dependent_id << " in new pipeline? " << dependent_in_new_pipeline << std::endl;
+      if (dependent_in_new_pipeline) {
+        // Start a new pipeline.
+        if (visited_nodes.find(dependent_id) == visited_nodes.end()) {
+          pipelines_.push_back(std::unique_ptr<Pipeline>(new Pipeline(dependent_id)));
+          to_be_checked_nodes.emplace(dependent_id, pipelines_.size() - 1);
+          // Link the two pipelines. dependent_id is the dependent of
+          // operator_id, which means the new pipeline is a dependent of the
+          // old pipeline.
+          const bool can_pipelines_be_fused =
+              canPipelinesBeFused(pipeline_id, pipelines_.size() - 1);
+          // std::cout << "For pipeline 94: " << pipeline_id << std::endl;
+          pipelines_[pipeline_id]->linkPipeline(
+              pipelines_.size() - 1,
+              operator_id,
+              true,
+              can_pipelines_be_fused);
+          // std::cout << "For pipeline 100: " << pipelines_.size() - 1 << std::endl;
+          pipelines_[pipelines_.size() - 1]->linkPipeline(
+              pipeline_id,
+              dependent_id,
+              false,
+              can_pipelines_be_fused);
+          visited_nodes[dependent_id] = true;
+          DLOG(INFO) << "New pipeline for dependent " << dependent_id << std::endl;
+        } else {
+          // We should find the pipelines of which dependent_id is a member.
+          auto pipelines_containing_dependent = getPipelineID(dependent_id);
+          // As dependent is in new pipeline, we are assured that we can connect
+          // pipelines in "pipelines_containing_dependent" with the current
+          // pipeline.
+          DLOG(INFO) << "Dependent " << dependent_id << " already visited" << std::endl;
+          for (auto i : pipelines_containing_dependent) {
+            DLOG(INFO) << "Pipeline (ID=" << pipeline_id << ") containing "
+                       << operator_id << " linked with pipeline (ID=" << i
+                       << "containing " << dependent_id << std::endl;
+            const bool can_pipelines_be_fused =
+                canPipelinesBeFused(pipeline_id, i);
+            // std::cout << "For pipeline 121: " << pipeline_id << std::endl;
+            pipelines_[pipeline_id]->linkPipeline(
+                i, operator_id, true, can_pipelines_be_fused);
+            // std::cout << "For pipeline 125: " << i << std::endl;
+            pipelines_[i]->linkPipeline(
+                pipeline_id, operator_id, false, can_pipelines_be_fused);
+          }
+        }
+      } else {
+        // This means that pipelining is enabled on this link. Add this
+        // dependent to the current pipeline being processed.
+        if (visited_nodes.find(dependent_id) == visited_nodes.end()) {
+          pipelines_[pipeline_id]->addOperatorToPipeline(dependent_id);
+          to_be_checked_nodes.emplace(dependent_id, pipeline_id);
+          visited_nodes[dependent_id] = true;
+          DLOG(INFO) << "Dependent " << dependent_id << " appended to pipeline "
+                     << pipeline_id << std::endl;
+        } else {
+          DLOG(INFO) << "Dependent " << dependent_id << " already visited"
+                     << std::endl;
+        }
+      }
+    }
+  }
+  // Make sure that all nodes belong to some pipeline exactly once.
+  DCHECK_EQ(query_plan_dag_->size(), getTotalNodes());
+}
+
+const std::size_t DAGAnalyzer::getTotalNodes() {
+  std::size_t total = 0;
+  for (std::size_t i = 0; i < pipelines_.size(); ++i) {
+    total += pipelines_[i]->size();
+  }
+  return total;
+}
+
+void DAGAnalyzer::visualizePipelines() {
+  std::vector<struct NodeInfo> pipelines_info;
+  std::vector<struct EdgeInfo> edges;
+  for (std::size_t id = 0; id < pipelines_.size(); ++id) {
+    plotSinglePipeline(id, &pipelines_info, &edges);
+  }
+  std::cout << "# nodes: " << pipelines_info.size()
+            << " # edges: " << edges.size() << std::endl;
+  std::cout << visualizePipelinesHelper(pipelines_info, edges) << std::endl;
+}
+
+std::string DAGAnalyzer::visualizePipelinesHelper(
+    const std::vector<struct NodeInfo> &pipelines_info,
+    const std::vector<struct EdgeInfo> &edges) {
+  // Format output graph
+  std::ostringstream graph_oss;
+  graph_oss << "strict digraph g {\n";
+  graph_oss << "  rankdir=LR\n";
+  graph_oss << "  node [penwidth=2]\n";
+  graph_oss << "  edge [fontsize=16 fontcolor=gray penwidth=2]\n\n";
+
+  // Format nodes
+  for (const auto &pipeline : pipelines_info) {
+    graph_oss << pipeline.id << " [ label= \"";
+    graph_oss << pipeline.labels;
+    graph_oss << "\"]\n";
+  }
+
+  // Format edges
+  for (const EdgeInfo &edge_info : edges) {
+    graph_oss << "  " << edge_info.src_node_id << " -> "
+              << edge_info.dst_node_id << " [ ";
+    if (edge_info.can_be_fused) {
+      // Pipelining is allowed over this edge.
+      graph_oss << "style=filled";
+    } else {
+      graph_oss << "style=dashed";
+    }
+    graph_oss << "]\n";
+  }
+  graph_oss << "}\n";
+  return graph_oss.str();
+}
+
+void DAGAnalyzer::plotSinglePipeline(
+    std::size_t pipeline_id,
+    std::vector<struct NodeInfo> *nodes,
+    std::vector<struct EdgeInfo> *edges) const {
+  NodeInfo current_node;
+  current_node.id = pipeline_id;
+  current_node.labels += "[";
+  current_node.labels += std::to_string(pipeline_id);
+  current_node.labels += "] Operators: ";
+  auto nodes_in_pipeline = pipelines_[pipeline_id]->getOperatorIDs();
+  for (auto node_id : nodes_in_pipeline) {
+    current_node.labels += std::to_string(node_id);
+    current_node.labels += "  ";
+  }
+  current_node.labels += "";
+  nodes->push_back(current_node);
+  auto neighbor_pipelines = pipelines_[pipeline_id]->getAllConnectedPipelines();
+  for (const PipelineConnection &neighbor : neighbor_pipelines) {
+    if (neighbor.checkPipelineIsDependent()) {
+      edges->emplace_back(pipeline_id,
+                          neighbor.getConnectedPipelineID(),
+                          neighbor.canPipelinesBeFused());
+    } else {
+      edges->emplace_back(neighbor.getConnectedPipelineID(),
+                          pipeline_id,
+                          neighbor.canPipelinesBeFused());
+    }
+  }
+}
+
+bool DAGAnalyzer::canPipelinesBeFused(const std::size_t src_pipeline_id,
+                                      const std::size_t dst_pipeline_id) const {
+  const std::size_t src_end_operator_id =
+      pipelines_[src_pipeline_id]->getPipelineEndPoint();
+  const std::size_t dst_start_operator_id =
+      pipelines_[dst_pipeline_id]->getPipelineStartPoint();
+  if (query_plan_dag_->hasLink(src_end_operator_id, dst_start_operator_id)) {
+    const bool is_pipeline_breaker =
+        query_plan_dag_->getLinkMetadata(src_end_operator_id, dst_start_operator_id);
+    DLOG(INFO) << "Link between " << src_end_operator_id << " and " << dst_start_operator_id << " is pipeline breaker? " << is_pipeline_breaker << std::endl;
+    return !is_pipeline_breaker;
+  } else {
+    DLOG(INFO) << "No Link between " << src_end_operator_id << " and " << dst_start_operator_id << std::endl;
+    return false;
+  }
+}
+
+}  // namespace quickstep
---
+++ query_execution/DAGAnalyzer.hpp
@@ -0,0 +1,284 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ **/
+
+#ifndef QUICKSTEP_QUERY_EXECUTION_DAG_ANALYZER_HPP_
+#define QUICKSTEP_QUERY_EXECUTION_DAG_ANALYZER_HPP_
+
+#include <cstddef>
+#include <memory>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+#include "query_execution/Pipeline.hpp"
+#include "relational_operators/RelationalOperator.hpp"
+#include "utility/DAG.hpp"
+#include "utility/Macros.hpp"
+
+#include "glog/logging.h"
+
+namespace quickstep {
+
+/** \addtogroup QueryExecution
+ *  @{
+ */
+
+/**
+ * @brief A class that processes a query plan DAG and produces pipelines of
+ *        relational operators from the DAG.
+ **/
+class DAGAnalyzer {
+ public:
+  /**
+   * @brief Constructor. The invocation of the constructor triggers the DAG
+   *        analysis and the pipelines are produced.
+   *
+   * @param query_plan_dag The query plan DAG.
+   **/
+  explicit DAGAnalyzer(DAG<RelationalOperator, bool> *query_plan_dag)
+      : query_plan_dag_(query_plan_dag) {
+    findPipelines();
+  }
+
+  /**
+   * @brief Get the number of pipelines in the DAG.
+   **/
+  const std::size_t getNumPipelines() const {
+    return pipelines_.size();
+  }
+
+  /**
+   * @brief Get the IDs of those pipelines of which operator_id is a part.
+   *
+   * @param operator_id The ID of the given operator.
+   *
+   * @return Pipeline IDs if such pipeline(s) exist.
+   **/
+  const std::vector<std::size_t> getPipelineID(const std::size_t operator_id) const {
+    std::vector<std::size_t> result_pipelines;
+    for (std::size_t pipeline_id = 0;
+         pipeline_id < pipelines_.size();
+         ++pipeline_id) {
+      if (pipelines_[pipeline_id]->hasOperator(operator_id)) {
+        result_pipelines.emplace_back(pipeline_id);
+      }
+    }
+    return result_pipelines;
+  }
+
+  /**
+   * @brief Check if pipeline has dependencies.
+   *
+   * @note This is a static check, i.e. it relies on the initial configuration
+   *       of the DAG and doesn't care about updates in the DAG.
+   *
+   * @param pipeline_id The ID of the pipeline.
+   *
+   * @return True if the pipeline has dependencies, false otherwise.
+   **/
+  bool checkPipelinehasDependenciesStatic(const std::size_t pipeline_id) const {
+    return !getPipelinesConnectedToStart(pipeline_id).empty();
+  }
+
+  /**
+   * @brief Get the pipelines that are connected to the start of the given
+   *        pipeline.
+   **/
+  std::unordered_set<std::size_t> getPipelinesConnectedToStart(
+      const std::size_t pipeline_id) const {
+    const std::size_t pipeline_start_node_id =
+        pipelines_[pipeline_id]->getPipelineStartPoint();
+    auto dependency_nodes = query_plan_dag_->getDependencies(pipeline_start_node_id);
+    auto pipelines_containing_dependency_nodes =
+        getPipelinesForNodes(dependency_nodes);
+    // Remove self pipeline_id from this set.
+    removeElementFromSet(&pipelines_containing_dependency_nodes, pipeline_id);
+    return pipelines_containing_dependency_nodes;
+  }
+
+  /**
+   * @brief Get the pipelines that are connected to the end of the given
+   *        pipeline.
+   **/
+  std::unordered_set<std::size_t> getPipelinesConnectedToEnd(
+      const std::size_t pipeline_id) const {
+    const std::size_t pipeline_end_node_id =
+        pipelines_[pipeline_id]->getPipelineEndPoint();
+    auto pipelines_containing_dependent_nodes = getPipelinesForNodes(
+        query_plan_dag_->getDependentsAsSet(pipeline_end_node_id));
+    // Remove self pipeline_id from this set.
+    removeElementFromSet(&pipelines_containing_dependent_nodes, pipeline_id);
+    return pipelines_containing_dependent_nodes;
+  }
+
+  /**
+   * @brief Get the pipelines which are connected (and end) at an intermediate
+   *        node of the given pipeline.
+   *
+   * @note This function is only relevant if there exists an intermediate node
+   *       in the pipeline, i.e. len(pipeline) > 2.
+   *
+   * @note Because of the condition that "a node with more than one dependencies
+   *       should belong to its own pipeline", this function will always result
+   *       empty vector.
+   **/
+  std::unordered_set<std::size_t> getPipelinesEndingAtIntermediateNode(
+      const std::size_t pipeline_id) {
+    std::unordered_set<std::size_t> result_pipelines;
+    return result_pipelines;
+  }
+
+  /**
+   * @brief Get the pipelines which are connected (and begin ) at an
+   *        intermediate node of the given pipeline.
+   *
+   * @note This function is only relevant if there exists an intermediate node
+   *       in the pipeline, i.e. len(pipeline) > 2.
+   **/
+  std::unordered_set<std::size_t> getPipelinesStartingAtIntermediateNode(
+      const std::size_t pipeline_id) {
+    std::unordered_set<std::size_t> result_pipelines;
+    if (pipelines_[pipeline_id]->size() > 2u) {
+      // Find the intermediate nodes.
+      auto all_nodes = pipelines_[pipeline_id]->getOperatorIDs();
+      std::unordered_set<std::size_t> intermediate_nodes;
+      auto begin_it = all_nodes.begin() + 1;
+      auto end_it = all_nodes.end() - 1;
+      intermediate_nodes.insert(begin_it, end_it);
+      result_pipelines = getPipelinesForNodes(intermediate_nodes);
+      removeElementFromSet(&result_pipelines, pipeline_id);
+    }
+    return result_pipelines;
+  }
+
+  /**
+   * @brief Get the pipelines that don't have a dependency pipeline.
+   *
+   * @note This is a static check, performed by disregarding the state of the DAG.
+   **/
+  std::vector<std::size_t> getFreePipelinesStatic() const {
+    std::vector<std::size_t> free_pipelines;
+    for (std::size_t id = 0; id < pipelines_.size(); ++id) {
+      if (!checkPipelinehasDependenciesStatic(id)) {
+        free_pipelines.emplace_back(id);
+      }
+    }
+    return free_pipelines;
+  }
+
+  const std::vector<std::size_t>& getAllOperatorsInPipeline(
+      const std::size_t pipeline_id) const {
+    DCHECK_LT(pipeline_id, pipelines_.size());
+    return pipelines_[pipeline_id]->getOperatorIDs();
+  }
+
+  const std::vector<std::size_t> getAllBlockingDependencies(
+      const std::size_t pipeline_id) const {
+    return pipelines_[pipeline_id]->getAllBlockingDependencies();
+  }
+
+  void visualizePipelines();
+
+ private:
+  /**
+   * @brief Information of a graph node.
+   */
+  struct NodeInfo {
+    std::size_t id;
+    std::string labels;
+    std::string color;
+  };
+
+  /**
+   * @brief Information of a graph edge.
+   */
+  struct EdgeInfo {
+    EdgeInfo(std::size_t src, std::size_t dst, bool can_edges_be_fused)
+        : src_node_id(src),
+          dst_node_id(dst),
+          can_be_fused(can_edges_be_fused) {}
+
+    std::size_t src_node_id;
+    std::size_t dst_node_id;
+    bool can_be_fused;
+  };
+
+  DAG<RelationalOperator, bool> *query_plan_dag_;
+  std::vector<std::unique_ptr<Pipeline>> pipelines_;
+
+  /**
+   * @brief Find initial set of pipelines in the query plan DAG.
+   **/
+  void findPipelines();
+
+  /**
+   * @brief Find the total number of nodes in all pipelines.
+   **/
+  const std::size_t getTotalNodes();
+
+  std::unordered_set<std::size_t> getPipelinesForNodes(
+      std::unordered_set<std::size_t> node_ids) const {
+    std::unordered_set<std::size_t> pipelines;
+    if (!node_ids.empty()) {
+      for (auto node_id : node_ids) {
+        auto pipelines_containing_node_id = getPipelineID(node_id);
+        pipelines.insert(pipelines_containing_node_id.begin(),
+                         pipelines_containing_node_id.end());
+      }
+    }
+    return pipelines;
+  }
+
+  void removeElementFromVector(std::vector<std::size_t> *vec,
+                               std::size_t elem_removed) const {
+    auto it = std::find(vec->begin(), vec->end(), elem_removed);
+    if (it != vec->end()) {
+      vec->erase(it);
+    }
+  }
+
+  void removeElementFromSet(std::unordered_set<std::size_t> *s,
+                            std::size_t elem_removed) const {
+    auto it = std::find(s->begin(), s->end(), elem_removed);
+    if (it != s->end()) {
+      s->erase(it);
+    }
+  }
+
+  void plotSinglePipeline(std::size_t pipeline_id,
+                          std::vector<struct NodeInfo> *nodes,
+                          std::vector<struct EdgeInfo> *edges) const;
+
+  std::string visualizePipelinesHelper(const std::vector<struct NodeInfo> &pipelines_info,
+                                const std::vector<struct EdgeInfo> &edges);
+
+  /**
+   * @brief Check if src pipeline be fused with dst pipeline.
+   **/
+  bool canPipelinesBeFused(const std::size_t src_pipeline_id,
+                           const std::size_t dst_pipeline_id) const;
+
+  DISALLOW_COPY_AND_ASSIGN(DAGAnalyzer);
+};
+
+/** @} */
+
+}  // namespace quickstep
+
+#endif  // QUICKSTEP_QUERY_EXECUTION_DAG_ANALYZER_HPP_
